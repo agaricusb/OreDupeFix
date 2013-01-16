0302ecfcc8e0bde8b4ed6c262b51abaa2e151d6a
Agaricus <agaricusb@yahoo.com>
2013-01-15 20:13:38 -0800

    Test replacing copper ingots in recipe outputs

--- build_oredupefix.sh
+++ build_oredupefix.sh
@@ -6,10 +6,15 @@ echo -------------------------------- Building OreDupeFix ----------------------
 ./reobfuscate.sh
 echo
 echo Adding release assets
-cp mcmod.info reobf/minecraft/
+cp ../mcmod.info reobf/minecraft/
+echo
+echo Removing API
+rm -rf reobf/minecraft/ic2
+rm -rf reobf/minecraft/thermalexpansion
 echo
 echo Archiving
 pushd reobf/minecraft
 zip -r ../../../oredupefix-snapshot.zip .
 popd
 popd
+cp oredupefix-snapshot.zip /Applications/MultiMC.app/Contents/Resources/instances/ModTest/minecraft/mods/
--- mcp/src/minecraft/agaricus/mods/oredupefix/OreDupeFix.java
+++ mcp/src/minecraft/agaricus/mods/oredupefix/OreDupeFix.java
@@ -10,6 +10,18 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+import ic2.core.AdvRecipe;
+import ic2.core.AdvShapelessRecipe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
+
+import java.util.List;
 
 @Mod(modid = "OreDupeFix", name = "OreDupeFix", version = "1.0")
 @NetworkMod(clientSideRequired = false, serverSideRequired = false)
@@ -17,6 +29,58 @@ public class OreDupeFix {
     @PostInit
     public static void postInit(FMLPostInitializationEvent event) {
         System.out.println("loading OreDupeFix!");
+
+        // TODO: load preferred ores from config
+
+        // Iterate recipes
+        List recipeList = CraftingManager.getInstance().getRecipeList();
+        for(Object recipe: recipeList) {
+            if (!(recipe instanceof IRecipe)) {
+                continue;
+            }
+
+            IRecipe iRecipe = (IRecipe)recipe;
+            ItemStack output = iRecipe.getRecipeOutput();
+
+            if (output == null) {
+                // some recipes require computing the output with getCraftingResult()
+                continue;
+            }
+
+            System.out.println("craft output: " + output.getDisplayName() + " = " + output.itemID + ":" + output.getItemDamage() + " class " + iRecipe.getClass());
+
+            int itemID = output.itemID;
+            int damage = output.getItemDamage();
+
+
+            if (itemID == 5267 && damage == 5) {  // RP2 copper
+                // TODO: set output
+                ItemStack newOutput = new ItemStack(20257, 1, 64); // TE
+                setRecipeOutput(iRecipe, newOutput);
+            }
+        }
+    }
+
+    public static void setRecipeOutput(IRecipe iRecipe, ItemStack output) {
+        if (iRecipe instanceof ShapedRecipes) {
+            ReflectionHelper.setPrivateValue(ShapedRecipes.class,(ShapedRecipes)iRecipe, output, "e"/*"recipeOutput"*/); // TODO: avoid hardcoding obf
+        } else if (iRecipe instanceof ShapelessRecipes) {
+            ReflectionHelper.setPrivateValue(ShapelessRecipes.class,(ShapelessRecipes)iRecipe, output, "a"/*"recipeOutput"*/);
+        } else if (iRecipe instanceof ShapelessOreRecipe) {
+            ReflectionHelper.setPrivateValue(ShapelessOreRecipe.class,(ShapelessOreRecipe)iRecipe, output, "output");
+        } else if (iRecipe instanceof ShapedOreRecipe) {
+            ReflectionHelper.setPrivateValue(ShapedOreRecipe.class,(ShapedOreRecipe)iRecipe, output, "output");
+
+        // IndustrialCraft^2
+        } else if (iRecipe instanceof AdvRecipe) {
+            // thanks IC2 for making this field public.. even if recipes aren't in the API
+            ((AdvRecipe)iRecipe).output = output;
+        } else if (iRecipe instanceof AdvShapelessRecipe) {
+            ((AdvShapelessRecipe)iRecipe).output = output;
+        }
+
+        // TODO: te
+        // TODO: bc
     }
 }
 
