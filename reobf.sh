b6005a47e24b2874beb000d48ff2df835819cb2d
Agaricus <agaricusb@yahoo.com>
2013-03-01 19:54:34 -0800

    Reobfuscate using new Maven SpecialSourceMP plugin

--- reobf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -x
-# Reobfuscate compiled mod pkgmcp->obf mappings
-# TODO: move to Maven
-java -jar ../SpecialSource/target/SpecialSource-1.3-SNAPSHOT-shaded.jar --in-jar target/OreDupeFix-2.0.jar --out-jar ../test-server/mods/oredupefix-2.0.jar --reverse --srg-in ../MinecraftForge/mcp/conf/
